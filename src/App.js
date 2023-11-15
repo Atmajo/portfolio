b749665e97ed0eb6acc797928da94341932f3c3f
Atmajo <atmajoc@gmail.com>
2023-11-15 06:45:17 +0530

    updates in App.jsx

--- src/App.js
+++ src/App.js
@@ -1,61 +1,92 @@
 import React, { useState } from "react";
-import {BsFillMoonStarsFill} from 'react-icons/bs';
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
-import deved from "./components/dev-ed-wave.png"
-import avatar from "./components/avatar.png"
-import paisa from "./components/paisa.mp3"
-import next from "./components/logos/nextjs.png"
-import react from "./components/logos/react.png"
-import mongo from "./components/logos/mongo.png"
-import js from "./components/logos/js.jpg"
-import html from "./components/logos/html.png"
-import git from "./components/logos/git.png"
-import css from "./components/logos/css.png"
-import c from "./components/logos/c.png"
-import boot from "./components/logos/boot.png"
-
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+} from "react-icons/ai";
+import avatar from "./components/avatar.png";
+import paisa from "./components/paisa.mp3";
+import deved from "./components/dev-ed-wave.png";
 function App() {
-  function sPlay(){
+  function sPlay() {
     const audio = new Audio(paisa);
     audio.play();
   }
   const [darkMode, setDarkMode] = useState(false);
   return (
-    <div className={darkMode? "dark":""}>
+    <div className={darkMode ? "dark" : ""}>
       <main className="select-none px-10 md:px-20 lg:px-40 dark:bg-gradient-to-b dark:from-slate-600 dark:to-slate-800  dark:text-white">
         <section className="min-h-screen">
           <nav className="dark:bg-slate-750 shadow-xl p-5 sm:p-10 rounded-xl py-10 mb-12 flex justify-between ">
             <h1 className="font-burtons text-2xl">Atmajo.</h1>
             <ul className="flex items-center">
-                <li><BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow cursor-pointer text-2xl"/></li>
-                <li className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow"><a href="https://drive.google.com/file/d/10qCm6pWe2Qq1KYyAIltYT8HKXVsksdiV/view?usp=sharing" className="hover:bg-purple-900 text-white px-4 py-2 bg-gradient-to-r from-purple-300 to-purple-500 rounded-md ml-8 ">Resume</a></li>
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow cursor-pointer text-2xl"
+                />
+              </li>
+              <li className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow">
+                <a
+                  href="https://drive.google.com/file/d/1nq_4nZRvQLWJgEji5zayjetrEA7iQ9AG/view?usp=sharing"
+                  className="hover:bg-purple-900 text-white px-4 py-2 bg-gradient-to-r from-purple-300 to-purple-500 rounded-md ml-8 "
+                >
+                  Resume
+                </a>
+              </li>
             </ul>
           </nav>
           <div className="relative mx-auto bg-gradient-to-b from-purple-300 to-purple-500 rounded-full w-80 h-80 mt-20 py-5">
-            <img src={deved} alt="deved" draggable="false"/>
+            <img src={deved} alt="deved" draggable="false" />
           </div>
           <div className="sm:text-center text-left sm:p-10 p-5">
-            <h2 className="text-6xl py-2 text-purple-700 dark:text-white">Atmajo Chowdhury</h2>
-            <h3 className="text-2xl py-2">Full-Stack Developer | UEMK ACM Secretary</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-white">Student at UEMK, studying B.Tech CSE in 2nd Year. Join me down below and let's begin cracking</p>
+            <h2 className="text-6xl py-2 text-purple-700 dark:text-white">
+              Atmajo Chowdhury
+            </h2>
+            <h3 className="text-2xl py-2">
+              Full-Stack Developer | UEMK ACM Secretary
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-white">
+              Student at UEMK, studying B.Tech CSE in 2nd Year. Join me down
+              below and let's begin cracking
+            </p>
           </div>
           <div className="flex text-5xl justify-center gap-5">
-            <a href="https://twitter.com/_atmajo_"><AiFillTwitterCircle className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow hover:rounded-full"/></a>
-            <a href="https://linkedin.com/in/AtmajoChowdhury"><AiFillLinkedin className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow hover:rounded-full" /></a>
-            <a href="https://github.com/atmajo"><AiFillGithub className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow hover:rounded-full" /></a>
+            <a href="https://twitter.com/_atmajo_">
+              <AiFillTwitterCircle className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow hover:rounded-full" />
+            </a>
+            <a href="https://linkedin.com/in/AtmajoChowdhury">
+              <AiFillLinkedin className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow hover:rounded-full" />
+            </a>
+            <a href="https://github.com/atmajo">
+              <AiFillGithub className="hover:drop-shadow-bglow dark:hover:drop-shadow-glow hover:rounded-full" />
+            </a>
           </div>
         </section>
         <section>
           <div>
             <h3 className="text-4xl py-1 text-center">Who am I ?</h3>
             <div className="relative mx-auto w-80 h-80 py-5">
-              <img src={avatar} alt="avatar" draggable="false"/>
+              <img src={avatar} alt="avatar" draggable="false" />
             </div>
-            <p className="text-2xl"><span className="font-semibold">Hola!</span>✌️ I am <span className="font-semibold">Atmajo Chowdhury</span>. </p>
-            <p className=" text-md py-7 text-gray-800 dark:text-white">I am a B.Tech Undergrad in Computer Science from University of Engineering and Management, Kolkata. My interest lies in Full-Stack Development, UI/UX Design and DSA.</p>
+            <p className="text-2xl">
+              <span className="font-semibold">Hola!</span>✌️ I am{" "}
+              <span className="font-semibold">Atmajo Chowdhury</span>.{" "}
+            </p>
+            <p className=" text-md py-7 text-gray-800 dark:text-white">
+              I am a B.Tech Undergrad in Computer Science from University of
+              Engineering and Management, Kolkata. My interest lies in
+              Full-Stack Development, UI/UX Design and DSA.
+            </p>
             <div className="">
               <h3 className="text-2xl">Engineering why ?</h3>
-              <button onClick={sPlay} className="rounded-lg bg-gradient-to-b from-purple-300 to-purple-500 p-2 my-5 px-5">Listen</button>
+              <button
+                onClick={sPlay}
+                className="rounded-lg bg-gradient-to-b from-purple-300 to-purple-500 p-2 my-5 px-5"
+              >
+                Listen
+              </button>
             </div>
           </div>
         </section>
@@ -64,72 +95,126 @@ function App() {
             <h3 className="text-4xl py-1 text-center">Skills</h3>
             <div className="dark:bg-slate-750 text-left shadow-lg p-10 rounded-xl flex gap-10 flex-col sm:flex-row md:gap-10 lg:justify-center">
               <div class="my-5">
-                  <h5 class="text-lg font-medium">Full-Stack Developer</h5>
-                  <p class="">I make cool looking websites and one of the example is this portfolio.</p>
+                <h5 class="text-lg font-medium">Full-Stack Developer</h5>
+                <p class="">
+                  I make cool looking websites and one of the example is this
+                  portfolio.
+                </p>
               </div>
               <div class="my-5">
-                  <h5 class="text-lg font-medium">Programmer</h5>
-                  <p class="">I solve DSA related codes everday as coding is my passion. </p>
+                <h5 class="text-lg font-medium">Programmer</h5>
+                <p class="">
+                  I solve DSA related codes everday as coding is my passion.{" "}
+                </p>
               </div>
             </div>
           </div>
           <div>
             <h3 className="text-4xl py-1 text-center mt-10">Education</h3>
             <div className="dark:bg-slate-750 text-left shadow-lg p-10 rounded-xl flex flex-col sm:flex-row md:gap-10 lg:justify-center">
-              
               <div class="my-5">
-                  <h5 class="text-xl font-medium">2011 - 2020</h5>
-                  <p class="">The Aryans School, Kolkata</p>
-                  <p class="italic">Class 2 to Class 10</p>
+                <h5 class="text-xl font-medium">2011 - 2020</h5>
+                <p class="">The Aryans School, Kolkata</p>
+                <p class="italic">Class 2 to Class 10</p>
               </div>
               <div class="my-5">
-                  <h5 class="text-xl font-medium">2020 - 2022</h5>
-                  <p class="">The Aryans School, Kolkata</p>
-                  <p class="italic">Class 11 to Class 12</p>
+                <h5 class="text-xl font-medium">2020 - 2022</h5>
+                <p class="">The Aryans School, Kolkata</p>
+                <p class="italic">Class 11 to Class 12</p>
               </div>
               <div class="my-5">
-                  <h5 class="text-xl font-medium">2022 - 2026</h5>
-                  <p class="">University of Engineering and Managemnt, Kolkata</p>
-                  <p class="italic">Computer Science and Engineering</p>
+                <h5 class="text-xl font-medium">2022 - 2026</h5>
+                <p class="">University of Engineering and Managemnt, Kolkata</p>
+                <p class="italic">Computer Science and Engineering</p>
               </div>
             </div>
           </div>
           <div>
             <h3 className="text-4xl py-1 text-center mt-10">Tools</h3>
-            <p className="text-center text-md py-2 text-gray-800 dark:text-white">What I use generally !</p>
+            <p className="text-center text-md py-2 text-gray-800 dark:text-white">
+              What I use generally !
+            </p>
             <div className="dark:bg-slate-750 text-left shadow-lg p-10 rounded-xl">
               <div className="flex justify-around py-10">
-                <div className="w-20 h-20"><img src={next} alt="" draggable="false"/></div>
-                <div className="w-20 h-20"><img src={react} alt="" draggable="false"/></div>
-                <div className="w-20 h-20"><img src={mongo} alt="" draggable="false"/></div>
+                <div className="w-20 h-20">
+                  <img src={`logos/nextjs.png`} alt="" draggable="false" />
+                </div>
+                <div className="w-20 h-20">
+                  <img src={`logos/react.png`} alt="" draggable="false" />
+                </div>
+                <div className="w-20 h-20">
+                  <img src={`logos/mongo.png`} alt="" draggable="false" />
+                </div>
               </div>
               <div className="flex justify-around py-10">
-                <div className="w-20 h-20"><img src={html} alt="" draggable="false"/></div>
-                <div className="w-20 h-20"><img src={css} alt="" draggable="false"/></div>
-                <div className="w-20 h-20"><img src={js} alt="" draggable="false"/></div>
+                <div className="w-20 h-20">
+                  <img src={`logos/html.png`} alt="" draggable="false" />
+                </div>
+                <div className="w-20 h-20">
+                  <img src={`logos/css.png`} alt="" draggable="false" />
+                </div>
+                <div className="w-20 h-20">
+                  <img src={`logos/js.png`} alt="" draggable="false" />
+                </div>
               </div>
               <div className="flex justify-around py-10">
-                <div className="w-20 h-20"><img src={git} alt="" draggable="false"/></div>
-                <div className="w-20 h-20"><img src={c} alt="" draggable="false"/></div>
-                <div className="w-20 h-20"><img src={boot} alt="" draggable="false"/></div>
+                <div className="w-20 h-20">
+                  <img src={`logos/git.png`} alt="" draggable="false" />
+                </div>
+                <div className="w-20 h-20">
+                  <img src={`logos/c.png`} alt="" draggable="false" />
+                </div>
+                <div className="w-20 h-20">
+                  <img src={`logos/boot.png`} alt="" draggable="false" />
+                </div>
               </div>
             </div>
           </div>
-          <div>
+          {/**<div>
             <h3 className="text-4xl py-1 text-center mt-10">Internships</h3>
             <div className="dark:bg-slate-750 text-left shadow-lg p-10 rounded-xl">
-            <div>
-              <div className="flex justify-center gap-5">
-                <div className=" py-5"><h3 className="text-2xl font-bold">2023</h3></div>
-                <div className="flex flex-col">
+              <div>
+                <div className="flex justify-center gap-5">
                   <div className=" py-5">
-                    <h3 className="text-2xl font-bold">Web Dev Intern</h3>
-                    <h4 className="text-lg">CodSoft</h4>
-                    <p className="italic">July,10 to August,10</p>
+                    <h3 className="text-2xl font-bold">2023</h3>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className=" py-5">
+                      <h3 className="text-2xl font-bold">Web Dev Intern</h3>
+                      <h4 className="text-lg">CodSoft</h4>
+                      <p className="italic">July,10 to August,10</p>
+                    </div>
                   </div>
                 </div>
               </div>
-          </div>
+            </div>
+          </div>**/}
+          <div className="text-center">
+            <h3 className="text-4xl py-1 text-center mt-10">Projects</h3>
+            <div className="dark:bg-slate-750 text-left shadow-lg p-10 rounded-xl">
+              <div>
+                <div className="flex justify-center gap-5">
+                  <div className=" py-5">
+                    <h3 className="text-2xl font-bold">2023</h3>
+                  </div>
+                  <div className="flex flex-col">
+                    <div className=" py-5">
+                      <h3 className="text-2xl font-bold">Portfolio</h3>
+                      <p className="italic">My personal website.</p>
+                    </div>
+                    <div className=" py-5">
+                      <h3 className="text-2xl font-bold">WeatherApp</h3>
+                      <p className="italic">An Weather App built with</p> 
+                      <p className="italic">geolocation services.</p>
+                    </div>
+                    <div className=" py-5">
+                      <h3 className="text-2xl font-bold">Login Site</h3>
+                      <p className="italic">Made with MERN Stack</p> 
+                      <p className="italic">to simulate basic authentication.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="text-center">
@@ -137,7 +222,9 @@ function App() {
             <div className="dark:bg-slate-750 text-left shadow-lg p-10 rounded-xl">
               <div>
                 <div className="flex justify-center gap-5">
-                  <div className=" py-5"><h3 className="text-2xl font-bold">2023</h3></div>
+                  <div className=" py-5">
+                    <h3 className="text-2xl font-bold">2023</h3>
+                  </div>
                   <div className="flex flex-col">
                     <div className=" py-5">
                       <h3 className="text-2xl font-bold">Volunteer</h3>
@@ -162,7 +249,12 @@ function App() {
         </section>
         <section>
           <footer className="py-5 text-center hover:drop-shadow-bglow dark:hover:drop-shadow-glow">
-            <h3>Copyright ©️<span><a href="/">Atmajo Chowdhury</a></span></h3>
+            <h3>
+              Copyright ©️
+              <span>
+                <a href="/">Atmajo Chowdhury</a>
+              </span>
+            </h3>
           </footer>
         </section>
       </main>
